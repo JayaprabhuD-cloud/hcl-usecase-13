00b200042616ea413720f013c62de112914911ca
JayaprabhuD-cloud <mail2jayaprabhu@gmail.com>
2025-06-19 17:32:28 +0530

    adding provider files

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "bayer-new-terraform-remote-state-bucket"
+    key    = "uc-12-new/terraform.tfstate"
+    region = "ap-south-1"   
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
